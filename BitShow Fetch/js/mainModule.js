c8870a97183191cd7c0d9c7a0e4e1c627dec8037
BIT Student <classroom@bgit.rs>
2018-03-13 16:24:41 +0100

    practice

---
+++ BitShow Fetch/js/mainModule.js
@@ -0,0 +1,150 @@
+import { createTVShows, searchedShows } from './TVShows.js';
+import {
+    displayShows,
+    showSearch,
+    displaySingleShow,
+    displayCast,
+    displaySeasons
+} from './UIModule.js';
+
+const initSingleShow = () => {
+
+    let singleShowId = localStorage.getItem("showId");
+    const singleShowRequest = `http://api.tvmaze.com/shows/${singleShowId}`;
+
+    fetch(singleShowRequest)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (show) {
+            displaySingleShow(show);
+        })
+
+
+    // singleShowRequest.done(response => {
+    //     displaySingleShow(response);
+    // })
+
+    const allSeasons = `http://api.tvmaze.com/shows/${singleShowId}/seasons`;
+
+    fetch(allSeasons)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (seasons) {
+            seasons.forEach(e => {
+                displaySeasons(e);
+            })
+        })
+
+    // allSeasons.done(response => {
+
+    //     response.forEach(response => {
+    //         displaySeasons(response);
+    //     })
+
+    // })
+
+    const cast = `http://api.tvmaze.com/shows/${singleShowId}/cast`;
+
+    fetch(cast)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (cast) {
+            cast.forEach(response => {
+                const { person } = response;
+                displayCast(person);
+            })
+        })
+
+    // cast.done(response => {
+
+    //     response.forEach(response => {
+    //         const { person } = response;
+    //         displayCast(person);
+    //     })
+    // })
+}
+
+const inputDropdown = () => {
+
+    $('#myInput').on('click', () => {
+        $('#myDropdown').toggleClass("show");
+    })
+
+    $('input').keyup(searched);
+}
+
+const allShows = () => {
+
+    const allShowsRequest = `http://api.tvmaze.com/shows`;
+
+    fetch(allShowsRequest)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (all) {
+            const shows = all.slice(0, 50);
+            shows.forEach(show => {
+                const singleShow = createTVShows(show.id, show.name, show.image.original);
+                displayShows(singleShow);
+            })
+            $('.showDiv').on('click', switchToSingleShow);
+        })
+
+    // allShowsRequest.done(response => {
+    //     const shows = response.slice(0, 50);
+
+    //     shows.forEach(show => {
+    //         const singleShow = createTVShows(show.id, show.name, show.image.original);
+    //         displayShows(singleShow);
+    //     })
+
+    //     $('.showDiv').on('click', switchToSingleShow);
+    // })
+}
+
+const searched = () => {
+
+    const searchedShow = $('input').val();
+
+    var request = `http://api.tvmaze.com/search/shows?q=${searchedShow}`;
+
+    fetch(request)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (searched) {
+            $('#myDropdown').text("");
+
+            const myShows = searchedShows(searched)
+            showSearch(myShows)
+
+            $('.searchList').on('click', switchToSingleShow);
+        })
+
+    // request.done(response => {
+    //     $('#myDropdown').text("");
+
+    //     const myShows = searchedShows(response)
+    //     showSearch(myShows)
+
+    //     $('.searchList').on('click', switchToSingleShow);
+    // });
+}
+
+function switchToSingleShow(event) {
+
+    const showId = $(this).attr('id');
+    localStorage.setItem('showId', showId);
+}
+
+
+export {
+    inputDropdown,
+    allShows,
+    switchToSingleShow,
+    searched,
+    initSingleShow
+};
--- promice-practice.js
+++ promice-practice.js
@@ -1,23 +1,51 @@
-// const myNewPromise = new Promise((resolve, reject) => {
-//     resolve(new Date());
-// })
+const myNewPromise = new Promise((resolve, reject) => {
+    resolve(new Date());
+})
 
-// myNewPromise.then((response) => {
-//     console.log(response);
-// })
+myNewPromise.then((response) => {
+    console.log(response);
+})
 
+//----------------------------------------------------
 const myArray = [];
 
 for (let i = 0; i < 10; i++) {
     myArray.push(new Promise((resolve, reject) => {
         const num = Math.random() * 3000;
-        
-        setTimeout(() => resolve(i), num);
+
+        setTimeout(() => {
+            if (num < 5000) {
+                resolve(i);
+            } else {
+                reject(i);
+            }
+
+        }, num);
     }));
 }
 
 myArray.map(promise => {
     promise.then(id => {
         console.log(id);
-    })
+    }, id => {
+        console.log(id + 'rejected');
+    });
+   
 });
+
+// myArray.map(promise => {
+//     promise.then(id => {
+//         console.log(id);
+//     });
+// });
+
+Promise.race(myArray).then((id) => {
+    console.log(id + 'first');
+});
+
+Promise.all(myArray).then((id) => {
+    console.log(id + 'completed');
+});
+
+
+
