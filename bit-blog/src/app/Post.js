b502357a4ef98f5b81ac7697a1a94c489569c4d0
BIT Student <classroom@bgit.rs>
2018-03-24 15:46:54 +0100

    not finished

--- bit-blog/src/app/Post.js
+++ bit-blog/src/app/Post.js
@@ -2,20 +2,42 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 
-const Post = (props) => {
-    return (
-        <div className="container">
-            <h4>SINGLE POST TITLE 1</h4>
-            <h4><Link to='/authors/:name'>Author Name</Link></h4>
-            <p className="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus obcaecati inventore enim debitis vel, dolore est itaque velit vero quisquam ea quasi amet, eos unde, pariatur perferendis. Ducimus accusantium laboriosam odio veniam enim, error officiis totam nesciunt facere ipsum minus ex distinctio, id quasi consequuntur eum corporis itaque dolorem ad eveniet? Ducimus ipsam quia non culpa fuga. Praesentium, ea! Vel voluptate, placeat voluptatibus nobis odit maiores eum non harum, recusandae aut libero? Consectetur, consequuntur corporis? Sit architecto fuga aperiam voluptates quaerat reiciendis voluptatem doloribus vitae officiis, labore expedita cumque eos vero, numquam eius, error nostrum veniam impedit repellendus voluptatibus nam!</p>
-            <h5>3 more posts from same author</h5>
-            <ul className="author-posts">
-                <li><Link to='/posts/:name'>Title 10</Link></li>
-                <li><Link to='/posts/:name'>Title 11</Link></li>
-                <li><Link to='/posts/:name'>Title 12</Link></li>
-            </ul>
-        </div>
-    )
+class Post extends React.Component {
+
+    constructor(props) {
+        super(props);
+        // this.state = {
+        //     allPosts: []
+        // }
+    }
+    // dva fetcha 
+
+    componentDidMount(){
+        // postList.fetchPosts()
+        //     .then((postsData) => {
+        //         this.setState({
+        //             allPosts: postsData
+        //         })
+        //     })
+
+        console.log(this.props.match.params.postId);
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h4>SINGLE POST TITLE 1</h4>
+                <h4><Link to='/authors/:name'>Author Name</Link></h4>
+                <p className="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus obcaecati inventore enim debitis vel, dolore est itaque velit vero quisquam ea quasi amet, eos unde, pariatur perferendis. Ducimus accusantium laboriosam odio veniam enim, error officiis totam nesciunt facere ipsum minus ex distinctio, id quasi consequuntur eum corporis itaque dolorem ad eveniet? Ducimus ipsam quia non culpa fuga. Praesentium, ea! Vel voluptate, placeat voluptatibus nobis odit maiores eum non harum, recusandae aut libero? Consectetur, consequuntur corporis? Sit architecto fuga aperiam voluptates quaerat reiciendis voluptatem doloribus vitae officiis, labore expedita cumque eos vero, numquam eius, error nostrum veniam impedit repellendus voluptatibus nam!</p>
+                <h5>3 more posts from same author</h5>
+                <ul className="author-posts">
+                    <li><Link to='/posts/:name'>Title 10</Link></li>
+                    <li><Link to='/posts/:name'>Title 11</Link></li>
+                    <li><Link to='/posts/:name'>Title 12</Link></li>
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default Post;
--- bit-blog/src/app/home_page/ListItemPost.js
+++ bit-blog/src/app/home_page/ListItemPost.js
@@ -6,7 +6,7 @@ import Home from './Home.js'
 const ListItemPost = (props) => {
     return (
         <li className='list-item'>
-            <h3><Link to='/posts/:name'>{props.value.title}</Link></h3>
+            <h3><Link to={`/posts/${props.value.postId}`}>{props.value.title}</Link></h3>
             <p>{props.value.body}</p>
         </li>
     )
